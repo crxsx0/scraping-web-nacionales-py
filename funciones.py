8464ef4479a165c7cfed4623a5930385d2b40d5d
crxsx0 <crxsx.0@gmail.com>
2024-02-01 16:50:11 -0300

    se añadieron 2 funciones para crear y revisar el estado de la tabla y dataset

--- funciones.py
+++ funciones.py
@@ -1,6 +1,8 @@
-import pandas_gbq, construmart, unicodedata
+import pandas_gbq, unicodedata
 import pandas as pd
 from google.cloud import storage
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
 
 '''Esta funcion se encarga de subir el dataframe a la tabla correspondiente en BigQuery. Es importante tener instalada la libreria de pandas_gbq. La cuenta de servicio que se utilizara para ejecutar tiene que tener permisos para usar BigQuery'''
 def subir_dataframe_bq(dataframe, ruta_tabla):
@@ -23,7 +25,7 @@ def subir_dfcsv_cstorage( dataframe_csv, ruta_destino_csv,  nombre_bucket):
         blob.upload_from_string(dataframe_csv, content_type='text/csv')
         return print('Se subio el csv exitosamente a Cloud Storage\n')
     except Exception as e:
-        return print('Hubo un error al subir el csv a Cloud Storage\n')
+        return print('Hubo un error al subir el csv a Cloud Storage\n'+ str(e) + '\n')
     
 def quitar_tildes(texto):
     """
@@ -68,3 +70,58 @@ async def extraer_lista_links(page, selector):
 def limpiar_formato_moneda(precio):
     # Elimina el símbolo de moneda y convierte a número
     return int(precio.replace('$', '').replace('.', ''))
+
+def creacion_tabla_bq (id_tabla):
+    client = bigquery.Client()
+
+    schema = [
+        bigquery.SchemaField("fecha", "DATE", description = "Fecha de extraccion"  ,mode="REQUIRED"),
+        bigquery.SchemaField("link", "STRING", description = "Link del producto",mode="REQUIRED"),
+        bigquery.SchemaField("tienda", "STRING", description = "Tienda a la que pertenece el producto",mode="REQUIRED"),
+        bigquery.SchemaField("precio", "INTEGER", description = "Precio del producto",mode="REQUIRED"),
+        bigquery.SchemaField("marca", "STRING", description = "Marca del producto",mode="REQUIRED"),
+        bigquery.SchemaField("descripcion", "STRING", description = "Descripcion del producto",mode="REQUIRED"),
+        bigquery.SchemaField("region", "STRING", description = "Region de la tienda",mode="REQUIRED"),
+        bigquery.SchemaField("comuna", "STRING", description = "Region de la comuna",mode="REQUIRED")
+    ]
+    
+    tabla = bigquery.Table(id_tabla, schema=schema)
+    tabla.labels = {
+        "enviroment": "dev",
+        "project-owner": "datos-arquitectura",
+        "state": "active",
+        "team": "marketing-maderas"
+    }
+    tabla.clustering_fields = ["fecha"]
+
+    tabla.time_partitioning = bigquery.TimePartitioning(
+        type_=bigquery.TimePartitioningType.DAY,
+        field="fecha",
+    ) 
+    tabla = client.create_table(tabla)
+
+def existencia_dataset_tabla(id_proyecto, id_dataset, id_tabla):
+    client = bigquery.Client(project=id_proyecto)
+    
+    dataset_full_id = f"{id_proyecto}.{id_dataset}"
+    table_full_id = f"{dataset_full_id}.{id_tabla}"
+    
+    # Verificar y crear el dataset si es necesario
+    try:
+        client.get_dataset(id_dataset)  # Intenta obtener el dataset
+        print(f"El dataset {dataset_full_id} ya existe.")
+    except NotFound:
+        print(f"El dataset {dataset_full_id} no existe, creándolo...")
+        dataset = bigquery.Dataset(dataset_full_id)
+        dataset.location = "US"  # Especifica la ubicación según sea necesario
+        client.create_dataset(dataset)
+        print(f"Dataset {dataset_full_id} creado con éxito.")
+    
+    # Verificar y crear la tabla si es necesario
+    try:
+        client.get_table(table_full_id)  # Intenta obtener la tabla
+        print(f"La tabla {table_full_id} ya existe.")
+    except NotFound:
+        print(f"La tabla {table_full_id} no existe, creándola...")
+        creacion_tabla_bq(id_tabla)
+        print(f"Tabla {table_full_id} creada con éxito.")
