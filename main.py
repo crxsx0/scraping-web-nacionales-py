8f3441133127123aa4eea37c1d4794225dfcade7
crxsx0 <crxsx.0@gmail.com>
2024-02-01 16:52:13 -0300

    se añadieron funciones a ejecutarse en main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import os, construmart, funciones, json
 from playwright.async_api import async_playwright
+from dotenv import load_dotenv
 from flask import Flask
 from datetime import datetime
 import pandas as pd
+#carga las variables de entorno
+load_dotenv()
 
 app = Flask(__name__)
 '''Esta funcion ejecuta la funcion asincrona main, se activa haciendo una peticion GET a la URL de Cloud Run'''
 @app.route("/")
 async def main():
     #Lee el archivo parametros.json
-    with open('tiendas.json', 'r', encoding='utf-8') as archivo:
+    with open('parametros.json', 'r', encoding='utf-8') as archivo:
         tiendas_construmart = json.load(archivo)
 
     #dia(hoy) de la extraccion
@@ -17,22 +20,25 @@ async def main():
 
     #Path para Cloud Storage y Big Query
     nombre_archivo = fecha_extraccion.strftime("%Y%m%d")+".csv"
-    nombre_bucket = 'cmpc-datos-precios-madera'
+    nombre_bucket = os.getenv("NOMBRE_BUCKET")
+    nombre_dataset = os.getenv("NOMBRE_DATASET")
+    nombre_proyecto = os.getenv("NOMBRE_PROYECTO")
     nombre_carpeta = 'construmart'
-    nombre_tabla = 'cmpc-datos-soluciones-sandbox.precios_madera.construmart'
     
     '''Esta funcion usa playwright de manera asincrona para ejecutar el scraping de cada pagina, luego crea el csv a partir de un dataframe lo sube a CLoud Storage y lo sube a Big Query'''
     async with async_playwright() as p:
         df_construmart = await construmart.scraping_tiendas(p, tiendas_construmart, fecha_extraccion)
-        df_csv = df_construmart.to_csv("prueba.csv", index= False)
+        df_csv = df_construmart.to_csv(index= False)
         ruta_csv = f'{nombre_carpeta}/{nombre_archivo}'
-        df_construmart['Fecha'] = pd.to_datetime(df_construmart['Fecha'])
+        ruta_dfbq = f'{nombre_proyecto}.{nombre_dataset}.construmart'
+        df_construmart['fecha'] = pd.to_datetime(df_construmart['fecha'])
         
         try:
+            funciones.existencia_dataset_tabla(nombre_proyecto, nombre_dataset, nombre_carpeta)
             #Se sube el dataframe en archivo csv a Cloud Storage
-            #funciones.subir_dfcsv_cstorage(df_csv, ruta_csv, nombre_bucket)
+            funciones.subir_dfcsv_cstorage(df_csv, ruta_csv, nombre_bucket)
             #Se sube el dataframe a BigQuery
-            #funciones.subir_dataframe_bq(df_construmart, nombre_tabla)
+            funciones.subir_dataframe_bq(df_construmart, ruta_dfbq)
             return 'Archivo CSV y DataFrame subido exitosamente.'
         except Exception as e:
             return f'Hubo un error: {str(e)}'
